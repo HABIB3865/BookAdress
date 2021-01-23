7c25ff4c21aa294f78d002ed5b3c30ae357e3be5
HABIB3865 <56259850+HABIB3865@users.noreply.github.com>
2021-01-23 14:52:38 +0700

    3A

---
+++ bookA/src/book/Book.java
@@ -0,0 +1,86 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package book;
+
+import book.model.DataPersonal;
+import java.io.IOException;
+ import java.util.logging.Level;
+ import java.util.logging.Logger;
+ import javafx.application.Application;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+ import javafx.fxml.FXMLLoader;
+ import javafx.scene.Scene;
+ import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.BorderPane;
+import javafx.stage.Stage;
+
+
+/**
+ *
+ * @author habib
+ */
+public class Book extends Application {
+    
+  //variabel kelas
+ private Stage stageAplikasi;
+ private BorderPane rootAplikasi;
+ private ObservableList<DataPersonal> dataperson = FXCollections.observableArrayList();
+
+public Book(){
+        dataperson.add(new DataPersonal("ahmad","habib"));
+        dataperson.add(new DataPersonal("saifulloh","habib"));
+        dataperson.add(new DataPersonal("abu","abu"));
+        dataperson.add(new DataPersonal("do","ma"));
+        dataperson.add(new DataPersonal("alif","ahmad"));
+ }
+
+ @Override
+ public void start(Stage primaryStage){
+ this.stageAplikasi = primaryStage;
+ this.stageAplikasi.setTitle("Buku Alamat");
+ initGuiKerangka(); //fungsi untuk inisialisasi node dari Guikerangka.fxml
+ initViewBook();//fungsi inisialisasi node dari viewBukuAlamat
+ }
+
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+    private void initGuiKerangka() {
+        try {
+        FXMLLoader loader = new FXMLLoader();
+        loader.setLocation(Book.class.getResource("view/GuiKerangka.fxml"));
+        rootAplikasi = (BorderPane)loader.load();
+
+        Scene sceneAplikasi = new Scene(rootAplikasi);
+        stageAplikasi.setScene(sceneAplikasi);
+        stageAplikasi.show();
+        } catch (IOException ex) {
+        Logger.getLogger(Book.class.getName()).log(Level.SEVERE, null, ex);
+    }
+
+    }
+    private void initViewBook() {
+        try {
+         FXMLLoader loader = new FXMLLoader();
+         loader.setLocation(Book.class.getResource(
+        "view/ViewBook.fxml"));
+         AnchorPane guiIsi = (AnchorPane)loader.load();
+        rootAplikasi.setCenter(guiIsi);
+         } catch (IOException ex) {
+         Logger.getLogger(Book.class.getName()).log(Level.SEVERE, null, ex);
+         }
+ }
+
+   
+
+    
+}
